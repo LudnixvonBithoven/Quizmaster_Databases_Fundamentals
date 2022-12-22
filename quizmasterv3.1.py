f0f99bfb1eabab729d0607f897d9abea1fad7f9b
Ludnix von Bithoven <117620996+LudnixvonBithoven@users.noreply.github.com>
2022-12-22 13:49:41 +0100

    Update quizmasterv3.1.py

--- quizmasterv3.1.py
+++ quizmasterv3.1.py
@@ -28,7 +28,9 @@ question3 = {"How do you insert COMMENTS in Python code?":\
 
 
 def convert_data(questions_list):
-    """convert_data(questions_list) -> str, list, str"""
+    """convert_data(questions_list) -> str, list, str
+    randomize the quiz sequence.
+    """
     rqstion = rnd.choices(questions_list)[0]
     question_key = list(rqstion.keys())[0]
     values_in_nested_list = list(rqstion.values())
@@ -40,32 +42,41 @@ def convert_data(questions_list):
 
 
 def question(questions_list):
-    
+    """ question_list --> list """
     rqstion, val_qstion_key, cval_key = convert_data(questions_list)
+    # print question
     print(rqstion)
+    # Returning randomized 3 answers in list 
     answers_in_list = rnd.sample(val_qstion_key, 3)
     if cval_key in answers_in_list:
         n = True
         while n:
+            # If the right answers already is in the answers list
             if cval_key in answers_in_list:
-                # Create a new random values list
+                # answers_in_list must have 3 randomized answers in list
                 answers_in_list = rnd.sample(val_qstion_key, 3)
             else:
-                answers_in_list.append(cval_key)
+                # exit the while loop
                 n = False
     else:
+        # Add the answer in the answers_in_list
         answers_in_list.append(cval_key)
         
+    # Shuffle the 4 answers in answers_in_list
     rnd.shuffle(answers_in_list)
 
+    # Print the values with the index number
+    # Index number is used for picking an answer
     for idx, ans in enumerate(answers_in_list):
         print(f"{idx}){ans}")
 
     try:
         answer_from_user = int(input("Answer: "))
     except ValueError as e:
+        # Except needs more attention and depth
         print(e)
     else:
+        # If input value is accepted, validate the answer
         if (cval_key.lower() 
                     == answers_in_list[answer_from_user].lower()):
             print(f"\n{answer_from_user} is correct\n")
@@ -77,6 +88,7 @@ def question(questions_list):
 n = True
 if __name__ == "__main__":
     while n:
+        # Trying a dictionary for creating a menu
         dct = {1: question([question1, question2, question3]), \
                2: False}
         try:

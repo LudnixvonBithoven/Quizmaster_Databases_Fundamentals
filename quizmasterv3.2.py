cc15a40b276d52121f0539c7a81ebd9c0d04743f
Ludnix von Bithoven <117620996+LudnixvonBithoven@users.noreply.github.com>
2022-12-25 01:49:31 +0100

    Update quizmasterv3.2.py

--- quizmasterv3.2.py
+++ quizmasterv3.2.py
@@ -1,31 +1,27 @@
 #######################################################################
 #                                                                     #
-#                      Quizmaster v3.2 by James                       #
-#                    Randomized questions and answers                 #
-#                   No repeating questions or answers                 #
+#                         Quizmaster v3.2 by James                    #
 #                                                                     #
 #######################################################################
 
 import random as rnd
 
 
-question1 = {"print([0, 1, [2, 3]][1:])) would result in?":\
-                ["[1, [2, 3]]", "[1]",\
+question1 = {"print([0, 1, [2, 3]][1:])) would result in?":
+                ["[1, [2, 3]]", "[1]", "[2, 3]", "[1, 2, 3]",
                  "typeError: 'int' object is not subscriptable",
-                 "[2, 3]",
-                 "DatatypeError: 'list' object is not subscriptable",\
-                 "[1, []]`", "[1, 2, 3]"]}
+                 "DatatypeError: 'list' object is not subscriptable",
+                 "[1, []]`"]}
 
-question2 = {"print(type({})) would result in?":\
-                ["<class 'dict'>", "<class 'set'>",\
-                 "<class 'list'>", "dict",\
-                 "{}", "```dict()```",\
+question2 = {"print(type({})) would result in?":
+                ["<class 'dict'>", "<class 'set'>",
+                 "<class 'list'>", "dict", "{}", "```dict()```",
                 "print('NameError: name '{}' is not defined')"]}
 
-question3 = {"How do you insert COMMENTS in Python code?":\
-                ["#This is a comment", "/*This is a comment*/",\
-                 "//This is comment", "<!-- This is a comment -->",\
-                 "###This is a comment", "```This is a comment```",\
+question3 = {"How do you insert COMMENTS in Python code?":
+                ["#This is a comment", "/*This is a comment*/",
+                 "//This is comment", "<!-- This is a comment -->",
+                 "###This is a comment", "```This is a comment```",
                 "print('This is a comment')"]}
 
 
@@ -48,7 +44,7 @@ def convert_data(questions_dict):
     return random_key, question, answers, correct_answer
 
 def question(questions_dict):
-    """question(questions_dict) -> dict
+    """question(questions_dict) --> dict
  
     Handeling the options
     Prompting answer from user
@@ -99,7 +95,7 @@ if __name__ == "__main__":
     # Note: this block needs a better looking logic
     questions_dict = {"q1": question1, "q2": question2, "q3": question3}
     while is_in_loop:
-        # If the questions dictionary isn't empty
+        # If there's a question in the dictionary
         if questions_dict:
             # Creating a menu with a dictionary
             menu_dict = {1: question(questions_dict), 2: False}
@@ -109,5 +105,4 @@ if __name__ == "__main__":
             except (KeyError, ValueError) as e:
                 print(e)
         else:
-            # Reload the questions dict if empty
             questions_dict = {"q1": question1, "q2": question2, "q3": question3}
